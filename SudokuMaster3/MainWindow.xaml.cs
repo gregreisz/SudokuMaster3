28a8a4e540350f7723c656df30bf69a11f020bcf
gregreisz <greg@reisz.net>
2018-06-25 23:07:57 -0500

    This is a stable build. I am working on rewriting code to work with WPF.

--- SudokuMaster3/MainWindow.xaml.cs
+++ SudokuMaster3/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace SudokuMaster3
 {
@@ -22,14 +19,84 @@ namespace SudokuMaster3
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private static string FilterOutReturns(string input)
         {
-            MessageBox.Show("This is a test of a button");
+            return !string.IsNullOrEmpty(input)
+                ? string.Join(string.Empty, Regex.Split(input, @"(?:\r\n|\n|\r|[ ])"))
+                : string.Empty;
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void InitializeBoard()
         {
+            var contents = FilterOutReturns(LoadGameFromDisk());
+            var counter = 0;
+            foreach (var row in Enumerable.Range(1, 9))
+            {
+                foreach (var col in Enumerable.Range(1, 9))
+                {
+                    var value = int.Parse(contents[counter++].ToString());
+                    var button = GetButtonByName($"cr{col}{row}");
+                    if (button != null)
+                    {
+                        button.Content = value > 0 ? value.ToString() : string.Empty;
+                        if (value > 0) button.Background = new SolidColorBrush(Colors.LightSteelBlue);
+                        if (col != 9)
+                        {
+                            textBox1.Text += $"{col}{row}{value}";
+                        }
+                        else
+                        {
+                            textBox1.Text += $"{col}{row}{value}" + Environment.NewLine;
+                        }
 
+                    }
+                }
+
+
+            }
         }
+
+        private Button GetButtonByName(string name)
+        {
+            var container = UniformGrid;
+            var btn = container.FindName(name);
+            if (btn is Button button)
+            {
+                return button;
+            }
+
+            return null;
+        }
+
+        private string LoadGameFromDisk()
+        {
+            var contents = string.Empty;
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = @"SDO files (*.sdo)|*.sdo|All files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = false
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Title = openFileDialog.FileName;
+                contents = File.ReadAllText(openFileDialog.FileName);
+            }
+
+            return contents;
+        }
+
+        private void ButtonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            InitializeBoard();
+        }
+
+        private void ButtonClearText_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = string.Empty;
+        }
+
     }
 }
