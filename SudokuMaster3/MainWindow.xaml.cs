b11eafca8dee4b320e71df763b58d4edf5e6bd77
gregreisz <greg@reisz.net>
2018-06-27 16:22:16 -0500

    I have the puzzle grid displaying and accepting input.

--- SudokuMaster3/MainWindow.xaml.cs
+++ SudokuMaster3/MainWindow.xaml.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Net.Mime;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Media;
 using Microsoft.Win32;
 
 namespace SudokuMaster3
 {
-    public partial class MainWindow : Window
+    public class CustomButton : Button
+    {
+        public bool IsLocked { get; set; }
+
+        public int? Value { get; set; }
+
+    }
+
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
         }
-
 
         private static string FilterOutReturns(string input)
         {
@@ -29,39 +34,90 @@ namespace SudokuMaster3
 
         private void InitializeBoard()
         {
-            var contents = FilterOutReturns(LoadGameFromDisk());
-            var counter = 0;
             foreach (var row in Enumerable.Range(1, 9))
             {
                 foreach (var col in Enumerable.Range(1, 9))
                 {
-                    var value = int.Parse(contents[counter++].ToString());
-                    var button = GetButtonByName($"cr{col}{row}");
+                    var button = GetCustomButtonByName($"cr{col}{row}");
                     if (button != null)
                     {
-                        button.Content = value > 0 ? value.ToString() : string.Empty;
-                        if (value > 0) button.Background = new SolidColorBrush(Colors.LightSteelBlue);
-                        if (col != 9)
-                        {
-                            textBox1.Text += $"{col}{row}{value}";
-                        }
-                        else
-                        {
-                            textBox1.Text += $"{col}{row}{value}" + Environment.NewLine;
-                        }
-
+                        button.Background = new SolidColorBrush(Colors.LightYellow);
                     }
                 }
-
-
             }
         }
 
-        private Button GetButtonByName(string name)
+
+        private void GetSavedGame()
         {
-            var container = UniformGrid;
+            var contents = FilterOutReturns(LoadGameFromDisk());
+            if (contents.Length != 81) return;
+
+            int counter = 0;
+            foreach (var row in Enumerable.Range(1, 9))
+            {
+                foreach (var col in Enumerable.Range(1, 9))
+                {
+                    var value = int.Parse(contents[counter].ToString());
+                    var button = GetCustomButtonByName($"cr{col}{row}");
+                    if (button != null)
+                    {
+                        if (value > 0)
+                        {
+                            button.IsLocked = true;
+                            button.Background = new SolidColorBrush(Colors.LightSteelBlue);
+                            button.Content = $"{value}";
+                        }
+                        else if (value == 0)
+                        {
+                            button.IsLocked = false;
+                            button.Background = new SolidColorBrush(Colors.LightYellow);
+                            button.ContextMenu = (ContextMenu)FindResource("contextMenu");
+                        }
+
+                        button.Value = value;
+                    }
+
+                    counter++;
+                }
+            }
+
+            var nl = Environment.NewLine;
+            textBox1.Text += contents.Substring(0, 9) + nl
+                                                      + contents.Substring(9, 9) + nl
+                                                      + contents.Substring(18, 9) + nl
+                                                      + contents.Substring(27, 9) + nl
+                                                      + contents.Substring(36, 9) + nl
+                                                      + contents.Substring(45, 9) + nl
+                                                      + contents.Substring(54, 9) + nl
+                                                      + contents.Substring(63, 9) + nl
+                                                      + contents.Substring(72, 9) + nl;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(((MenuItem)sender).Header.ToString().Substring(5));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is MenuItem mi)
+            {
+                if (mi.CommandParameter is ContextMenu cm)
+                {
+                    if (cm.PlacementTarget is CustomButton button)
+                    {
+                        button.Content = mi.Header.ToString().Substring(5, 1);
+                    }
+                }
+            }
+        }
+
+        private CustomButton GetCustomButtonByName(string name)
+        {
+            var container = PuzzleGrid;
             var btn = container.FindName(name);
-            if (btn is Button button)
+            if (btn is CustomButton button)
             {
                 return button;
             }
@@ -90,7 +146,7 @@ namespace SudokuMaster3
 
         private void ButtonOpenFile_Click(object sender, RoutedEventArgs e)
         {
-            InitializeBoard();
+            GetSavedGame();
         }
 
         private void ButtonClearText_Click(object sender, RoutedEventArgs e)
